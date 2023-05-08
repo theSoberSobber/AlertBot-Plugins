e85ba06faef79a5383abbc8fec759b94cc37da71
theSoberSobber <akshatshrivastava0010@gmail.com>
2023-05-09 00:33:11 +0530

    structure

--- releaseApi/index.ts
+++ releaseApi/index.ts
@@ -1,15 +1,56 @@
 import express, { Express, Request, Response } from 'express';
-// import dotenv from 'dotenv';
-
-// dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
+app.get('/plugins', (req: Request, res: Response) => {
+  res.json({
+    base: "https://raw.githubusercontent.com/theSoberSobber/AlertBot-Plugins/main/plugins",
+    // nah download from releases
+    plugins: [
+      {
+        name: "abcd.zip",
+        sha256: "yufvbgihnoj"
+      }
+      // this array is sorted on basis of plugins.name
+    ]
+  });
 });
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
+
+// JSON FOR PLUGINS?
+//
+// {
+//   base: "https://raw.githubusercontent.com/theSoberSobber/AlertBot-Plugins/main/plugins",
+//   plugins: [
+//     {
+//       name: "abcd.zip",
+//       sha256: "yufvbgihnoj"
+//     }
+//     // this array is sorted on basis of plugins.name
+//   ]
+// }
+// 
+// OR
+//
+// {
+//   base: "https://raw.githubusercontent.com/theSoberSobber/AlertBot-Plugins/main/plugins",
+//   plugins: {
+//     sortedKeyName: {
+//       name: "sortedKeyName",
+//       fileTree: [
+//         [
+//           "parsers", [
+//             "parser1", "parser2", "parser3"
+//           ]
+//         ],
+//         "getUpdates.js",
+//         "map.js",
+//         "updateHandler.js"
+//       ]
+//     }
+//   }
+// }
